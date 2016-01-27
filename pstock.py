2f98f853db9801f42f9e95a757fb2ebf112ba2bb
Tian Luo <tian.luo@purestorage.com>
2016-01-27 13:00:55 -0800

    check current prices

--- pstock.py
+++ pstock.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python
 
-print 'hello world'
+from yahoo_finance import Share
+
+def read_watchlist() :
+    return [line.strip() for line in open("watchlist.txt", 'r')]
+
+def get_current_prices(watchlist) :
+    return {symbol : Share(symbol).get_price() for symbol in watchlist }
+
+def main() :
+    watchlist = read_watchlist()
+    print watchlist
+    current_prices = get_current_prices(watchlist)
+    print current_prices
+
+if __name__ == '__main__' :
+    main()
