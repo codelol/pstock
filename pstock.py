35a66624df2013486639eb6068e191d6c324e036
Tian Luo <tian.luo@purestorage.com>
2016-01-27 22:25:58 -0800

    Figure out latest trading day and get the correct history

--- pstock.py
+++ pstock.py
@@ -9,16 +9,34 @@ def read_watchlist() :
 def get_current_prices(symbols) :
     return {sym : Share(sym).get_price() for sym in symbols }
 
-# XXX history from 80 days ago until previous trading day
-def get_history(symbols) :
-    # XXX today should be today is it's a trading day, or the last trading day
-    # XXX prev_trading_day is the further previous trading day
-    prev_trading_day = datetime.now() - timedelta(days = 1)
-    date_80_days_ago = prev_trading_day - timedelta(days = 80)
-    start = date_80_days_ago.date().isoformat()
-    end   = prev_trading_day.date().isoformat()
+def get_all_history(symbols) :
+    def get_latest_trading_day() :
+        # return today if it is a trading day, otherwise return the most recent one
+        today = datetime.today()
+        return {
+            6 : today - timedelta(days = 2), # Sunday, return Friday
+            5 : today - timedelta(days = 1), # Saturday, return Friday
+                                             # Monday to Friday is [0, 4]
+        }.get(today.weekday(), today)
+
+    latest_trading_day = get_latest_trading_day()
+
+    history_starting_day = latest_trading_day - timedelta(days = 20)
+    history_ending_day = latest_trading_day - timedelta(days = 1)
+    start = history_starting_day.date().isoformat()
+    end   = history_ending_day.date().isoformat()
 
     return {sym : Share(sym).get_historical(start, end) for sym in symbols}
+
+def get_history_close_prices(full_history):
+    history_close_prices = {}
+    for sym in full_history.keys() :
+        info = full_history[sym]
+        prices = []
+        for each_day in info:
+            prices.append(each_day['Close'])
+        history_close_prices[sym] = prices
+    return history_close_prices
 
 def main() :
     watchlist = read_watchlist()
@@ -27,8 +45,11 @@ def main() :
     current_prices = get_current_prices(watchlist)
     print current_prices
 
-    history = get_history(watchlist)
-    print history
+    full_history = get_all_history(watchlist)
+    print full_history
+
+    history_close_prices = get_history_close_prices(full_history)
+    print history_close_prices
 
 if __name__ == '__main__' :
     main()
