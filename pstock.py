6ee2e5f5105246fc0b43c8b1cbc5f85166ea9c24
Tian Luo <jackrobin@gmail.com>
2016-01-28 23:38:37 -0800

    minor format tweak

--- pstock.py
+++ pstock.py
@@ -9,11 +9,13 @@ def read_watchlist() :
     return [line.strip() for line in open("watchlist.txt", 'r')]
 
 def get_current_prices(latest_data, symbols) :
+    print 'Requesting latest:', datetime.now().isoformat()
     for sym in symbols :
         # use Google API for no-delay quotes
         latest_data[sym]['price'] = getQuotes(sym)[0]['LastTradePrice']
 
 def get_all_history(symbols) :
+    print 'requesting history data'
     def get_latest_trading_day() :
         # return today if it is a trading day, otherwise return the most recent one
         today = datetime.today()
@@ -24,7 +26,6 @@ def get_all_history(symbols) :
         }.get(today.weekday(), today)
 
     latest_trading_day = get_latest_trading_day()
-    print 'Latest trading day:',latest_trading_day.date().isoformat()
 
     history_starting_day = latest_trading_day - timedelta(days = 40)
     history_ending_day = latest_trading_day - timedelta(days = 1)
@@ -44,6 +45,7 @@ def get_all_history(symbols) :
     for sym in symbols :
         ret[sym]['close_prices'] = history_close_prices[sym]
 
+    print 'received history data'
     return ret
 
 def cal_simple_moving_average(latest_data, prices, days) :
@@ -59,7 +61,7 @@ def merge_current_and_history_close_prices(symbols, latest_data, full_history) :
 
 
 def print_results(symbols, latest_data, full_history) :
-    headers = ['Symbol', 'Price', 'Change', 'Change%', 'sma5 - sma10', 'sma5 - sma20']
+    headers = ['Symbol', 'Price', 'Change', 'Change%', 'sma(5 - 10)', 'sma(5 - sma20)']
     rows = []
     for sym in symbols :
         r = []
@@ -67,7 +69,7 @@ def print_results(symbols, latest_data, full_history) :
         r.append(sym)
         r.append(latest_data[sym]['price'])
         r.append('{0:+.2f}'.format(price_change))
-        r.append('{0:+.2%}'.format(price_change / float(full_history[sym]['close_prices'][0])))
+        r.append('{0:+.2f}'.format(price_change * 100 / float(full_history[sym]['close_prices'][0])))
         r.append('{0:+.2f}'.format(float(latest_data[sym]['sma5']) - float(latest_data[sym]['sma10'])))
         r.append('{0:+.2f}'.format(float(latest_data[sym]['sma5']) - float(latest_data[sym]['sma20'])))
         rows.append(r)
