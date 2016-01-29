d50600b780ebaf39d82fd05438b50b1bdd6a9c3e
Tian Luo <jackrobin@gmail.com>
2016-01-29 11:37:46 -0800

    wrap analysis code in a class and use time loop

--- pstock.py
+++ pstock.py
@@ -60,43 +60,57 @@ def merge_current_and_history_close_prices(symbols, latest_data, full_history) :
     return merged_prices
 
 
-def print_results(symbols, latest_data, full_history) :
-    headers = ['Symbol', 'Price', 'Change', 'Change%', 'sma(5 - 10)', 'sma(5 - sma20)']
-    rows = []
-    for sym in symbols :
-        r = []
-        price_change = float(latest_data[sym]['price']) - float(full_history[sym]['close_prices'][0])
-        r.append(sym)
-        r.append(latest_data[sym]['price'])
-        r.append('{0:+.2f}'.format(price_change))
-        r.append('{0:+.2f}'.format(price_change * 100 / float(full_history[sym]['close_prices'][0])))
-        r.append('{0:+.2f}'.format(float(latest_data[sym]['sma5']) - float(latest_data[sym]['sma10'])))
-        r.append('{0:+.2f}'.format(float(latest_data[sym]['sma5']) - float(latest_data[sym]['sma20'])))
-        rows.append(r)
-    print tabulate(rows, headers)
+class TA:
+    def __init__(self, watchlist, full_history):
+        self.symbols = watchlist
+        self.latest_data = {sym : {} for sym in watchlist}
+        self.full_history = full_history
+        self.interests_sma = [5, 10, 20]
 
+    def analysis(self):
+        # put latest info into latest_data
+        get_current_prices(self.latest_data, self.symbols)
 
-def analysis(watchlist, full_history, latest_data) :
-    # put latest info into latest_data
-    get_current_prices(latest_data, watchlist)
+        merged_prices = merge_current_and_history_close_prices(self.symbols,
+                                                               self.latest_data,
+                                                               self.full_history)
 
-    merged_prices = merge_current_and_history_close_prices(watchlist, latest_data, full_history)
+        cal_simple_moving_average(self.latest_data, merged_prices, 5)
+        cal_simple_moving_average(self.latest_data, merged_prices, 10)
+        cal_simple_moving_average(self.latest_data, merged_prices, 20)
 
-    cal_simple_moving_average(latest_data, merged_prices, 5)
-    cal_simple_moving_average(latest_data, merged_prices, 10)
-    cal_simple_moving_average(latest_data, merged_prices, 20)
+    def print_results(self):
+        headers = ['Symbol', 'Price', 'Change', 'Change%', 'sma(5 - 10)', 'sma(5 - sma20)']
+        rows = []
+        for sym in self.symbols :
+            r = []
+            price_change = float(self.latest_data[sym]['price']) - float(self.full_history[sym]['close_prices'][0])
+            r.append(sym)
+            r.append(self.latest_data[sym]['price'])
+            r.append('{0:+.2f}'.format(price_change))
+            r.append('{0:+.2f}'.format(price_change * 100 / float(self.full_history[sym]['close_prices'][0])))
+            r.append('{0:+.2f}'.format(float(self.latest_data[sym]['sma5']) - float(self.latest_data[sym]['sma10'])))
+            r.append('{0:+.2f}'.format(float(self.latest_data[sym]['sma5']) - float(self.latest_data[sym]['sma20'])))
+            rows.append(r)
+        print tabulate(rows, headers)
 
-    print_results(watchlist, latest_data, full_history)
+    def loop(self):
+        import time
+        while True:
+            self.analysis()
+            self.print_results()
+            print '============++++++++============'
+            time.sleep(60)
 
 def main() :
     watchlist = read_watchlist()
-    latest_data = {sym : {} for sym in watchlist}
     print watchlist
 
     # put history info into full_history
     full_history = get_all_history(watchlist)
 
-    analysis(watchlist, full_history, latest_data)
+    ta = TA(watchlist, full_history)
+    ta.loop()
 
 if __name__ == '__main__' :
     main()
