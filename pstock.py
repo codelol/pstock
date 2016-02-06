a788518901696832bd45824bbb5635cc7ac7b5ee
Tian Luo <jackrobin@gmail.com>
2016-02-05 16:38:29 -0800

    code refactor

--- pstock.py
+++ pstock.py
@@ -28,51 +28,39 @@ def get_all_history(symbols) :
     start = history_starting_day.date().isoformat()
     end   = history_ending_day.date().isoformat()
 
-    full_history = {sym : Share(sym).get_historical(start, end) for sym in symbols}
-    ret = {sym : {} for sym in symbols}
-
-    def get_history_close_prices(full_history):
-        history_close_prices = {}
-        for sym in full_history.keys() :
-            history_close_prices[sym] = [x['Close'] for x in full_history[sym]]
-        return history_close_prices
-
-    history_close_prices = get_history_close_prices(full_history)
-    for sym in symbols :
-        ret[sym]['close_prices'] = history_close_prices[sym]
-
+    ret = {sym : [{}] + Share(sym).get_historical(start, end) for sym in symbols}
     print 'received history data'
     return ret
-
 
 class TA:
     def __init__(self, watchlist, full_history):
         self.symbols = watchlist
-        self.latest_data = {sym : {} for sym in watchlist}
+        self.aggregates = {sym : {} for sym in watchlist}
         self.full_history = full_history
         self.interests_sma = [5, 10, 20]
         self.rules = [self.rule_sma_crossing]
 
-    def get_current_prices(self):
-        print 'Requesting current prices:', datetime.now().isoformat()
+    def get_latest(self):
+        print 'Requesting latest info:', datetime.now().isoformat()
+        errmsg = ''
         for sym in self.symbols :
-            # use Google API for no-delay quotes
-            self.latest_data[sym]['price'] = getQuotes(sym)[0]['LastTradePrice']
-
-    def merge_current_and_history_close_prices(self) :
-        merged_prices = {}
-        for sym in self.symbols :
-            merged_prices[sym] = [self.latest_data[sym]['price']] + self.full_history[sym]['close_prices']
-        self.merged_prices = merged_prices
+            try:
+                # use Google API for no-delay quotes
+                self.full_history[sym][0]['Close'] = getQuotes(sym)[0]['LastTradePrice']
+            except:
+                # or Yahoo if Google is not available
+                ydata = Share(sym)
+                self.full_history[sym][0]['Close'] = ydata.get_price()
+                errmsg = 'Google API error: ' + str(sys.exc_info()[0])
+        return errmsg
 
     def cal_simple_moving_average(self) :
         for sym in self.symbols :
             for days in self.interests_sma :
-                self.latest_data[sym]['sma'+str(days)] = sum([float(x) for x in self.merged_prices[sym][0:days]]) / days
-                self.latest_data[sym]['sma'+str(days)+'_prev'] = sum([float(x) for x in self.merged_prices[sym][1:days+1]]) / days
+                self.aggregates[sym]['sma'+str(days)] = sum([float(x['Close']) for x in self.full_history[sym][0:days]]) / days
+                self.aggregates[sym]['sma'+str(days)+'_prev'] = sum([float(x['Close']) for x in self.full_history[sym][1:days+1]]) / days
 
     def calculations(self):
-        self.merge_current_and_history_close_prices()
         self.cal_simple_moving_average()
 
     def rule_sma_crossing(self, sym):
@@ -80,14 +68,14 @@ class TA:
         day2 = self.interests_sma[1]
         day3 = self.interests_sma[2]
 
-        cur_sma1 = self.latest_data[sym]['sma'+str(day1)]
-        pre_sma1 = self.latest_data[sym]['sma'+str(day1)+'_prev']
+        cur_sma1 = self.aggregates[sym]['sma'+str(day1)]
+        pre_sma1 = self.aggregates[sym]['sma'+str(day1)+'_prev']
 
-        cur_sma2 = self.latest_data[sym]['sma'+str(day2)]
-        pre_sma2 = self.latest_data[sym]['sma'+str(day2)+'_prev']
+        cur_sma2 = self.aggregates[sym]['sma'+str(day2)]
+        pre_sma2 = self.aggregates[sym]['sma'+str(day2)+'_prev']
 
-        cur_sma3 = self.latest_data[sym]['sma'+str(day3)]
-        pre_sma3 = self.latest_data[sym]['sma'+str(day3)+'_prev']
+        cur_sma3 = self.aggregates[sym]['sma'+str(day3)]
+        pre_sma3 = self.aggregates[sym]['sma'+str(day3)+'_prev']
 
         cur_sma1_minus_cur_sma2 = cur_sma1 - cur_sma2
         pre_sma1_minus_pre_sma2 = pre_sma1 - pre_sma2
@@ -139,22 +127,25 @@ class TA:
         rows = []
         for sym in self.symbols :
             r = []
-            price_change = float(self.latest_data[sym]['price']) - float(self.full_history[sym]['close_prices'][0])
+            price_change = float(self.full_history[sym][0]['Close']) - float(self.full_history[sym][1]['Close'])
             r.append(sym)
-            r.append(self.latest_data[sym]['price'])
-            r.append('{0:+.2f}'.format(price_change * 100 / float(self.full_history[sym]['close_prices'][0])))
+            r.append(float(self.full_history[sym][0]['Close']))
+            r.append('{0:+.2f}'.format(price_change * 100 / float(self.full_history[sym][1]['Close'])))
             r.append('{0:+.2f}'.format(price_change))
-            r.append('{0:+.2f}'.format(float(self.latest_data[sym]['sma'+str(day1)]) - float(self.latest_data[sym]['sma'+str(day2)])))
-            r.append('{0:+.2f}'.format(float(self.latest_data[sym]['sma'+str(day1)]) - float(self.latest_data[sym]['sma'+str(day3)])))
-            r.append('{0:+.2f}'.format(float(self.latest_data[sym]['sma'+str(day2)]) - float(self.latest_data[sym]['sma'+str(day3)])))
+            r.append('{0:+.2f}'.format(float(self.aggregates[sym]['sma'+str(day1)]) - float(self.aggregates[sym]['sma'+str(day2)])))
+            r.append('{0:+.2f}'.format(float(self.aggregates[sym]['sma'+str(day1)]) - float(self.aggregates[sym]['sma'+str(day3)])))
+            r.append('{0:+.2f}'.format(float(self.aggregates[sym]['sma'+str(day2)]) - float(self.aggregates[sym]['sma'+str(day3)])))
             rows.append(r)
         print tabulate(rows, headers)
 
     def loop(self):
-        sleep_time = 60
+        sleep_time = 120
         while True:
             try:
-                self.get_current_prices()
+                errmsg = self.get_latest()
+                if len(errmsg) > 0:
+                    print errmsg
+
                 self.calculations()
                 self.print_results()
                 self.run_rules()
