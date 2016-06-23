e9e361e36537830ba7037394b470f2551d001f61
Tian Luo <jackrobin@gmail.com>
2016-06-23 06:41:34 -0700

    rsi calculation

--- ptools/metrics.py
+++ ptools/metrics.py
@@ -37,8 +37,47 @@ class Metrics:
             index1.append((closePrices[i] - closePrices[i+1] * volumes[i]))
         return self.ema(index1, d)
 
+    def rsi(self, datapoints, d = 14):
+        if (len(datapoints) < d + 1):
+            return None
+        initialGain = initialLoss = 0
+        for i in range(1, d):
+            if datapoints[i] > datapoints[i-1]:
+                initialGain += datapoints[i] - datapoints[i-1]
+            elif datapoints[i] < datapoints[i-1]:
+                initialLoss += datapoints[i-1] - datapoints[i]
+        gain = loss = 0
+        RSarray = []
+        for i in range(d, len(datapoints)):
+            curGain = curLoss = 0
+            if datapoints[i] > datapoints[i-1]:
+                curGain += datapoints[i] - datapoints[i-1]
+            elif datapoints[i] < datapoints[i-1]:
+                curLoss += datapoints[i-1] - datapoints[i]
+            if i == d:
+                gain = (initialGain + curGain) / d
+                loss = (initialLoss + curLoss) / d
+            else:
+                gain = (gain * (d-1) + curGain) / d
+                loss = (loss * (d-1) + curLoss) / d
+            RSarray.insert(0, gain / loss)
+
+        RSIarray = [100 - 100 / (1 + x) for x in RSarray]
+        return RSIarray
+
+
+def testRSI():
+    data = [44.34, 44.09, 44.15, 43.61, 44.33, 44.83, 45.10, 45.42, 45.84, 46.08, 45.89, 46.03, 45.61, 46.28,
+            46.28, 46.00, 46.03]
+    result = (Metrics().rsi(data))
+    assert(len(result) == 3)
+    assert(result[0] > 66.480 and result[0] < 66.490)
+    assert(result[1] > 66.240 and result[1] < 66.250)
+    assert(result[2] > 70.460 and result[0] < 70.470)
+    print('testRSI passed:' + str(result))
+
 def main():
-    sym = 'AAPL'
+    sym = 'GOOGL'
     watchlist = [sym]
     print(watchlist)
     marketData = USMarket(watchlist)
@@ -50,8 +89,9 @@ def main():
     # macd_histo = mts.macd(closePrices)
     # print(str(macd_histo))
 
-    forceIndex = mts.forceIndex(closePrices, volumes)
-    print(str(forceIndex))
+    # forceIndex = mts.forceIndex(closePrices, volumes)
+    # print(str(forceIndex))
 
 if __name__ == '__main__' :
     main()
+    # testRSI()
