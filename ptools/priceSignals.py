3faddfac78ec1a427d27ca9cda88e65fd4e602e6
Tian Luo <tian.luo@purestorage.com>
2016-07-27 22:18:13 -0700

    reduce noise of type 1 buy point

--- ptools/priceSignals.py
+++ ptools/priceSignals.py
@@ -18,15 +18,17 @@ class PriceSignals:
     0. 当前价格必须低于上一个价格, macdh是绿柱子,而且比上一根短
     1. 找出从今到之前的三个区间:
         区间1: 截止至今天的区间,macd-h是负数
-        区间2: 之前的一个区间,macd-h是整数
+        区间2: 之前的一个区间,macd-h是正数
         区间3: 再往前一个拒签,macd-h是负数
         如果找不到这三个区间,那么数据不足,不能继续
+        #如果任何区间的长度少于等于3，那么可以认为是噪音，不考虑
     2. 计算区间1到区间3的最低收盘价,当前的价格应该是最低,才能继续
        可以加如下条件进一步过滤结果: 计算三个区间的最高价,今天的价格必须低于最高价20%以上
     3. 计算区间1和区间3的macd-h最小值,如果区间1的最小值大于区间3的, 那么继续
     4. 如果到这里,那么bullish divergence应该成立,返回True
     """
     def Type1_buy_point_MACD_bullish_divergence(self, closePrices):
+        min_sec_len = 3
         dsize = len(closePrices)
         #Step 0
         if closePrices[0] > closePrices[1]:
@@ -41,19 +43,19 @@ class PriceSignals:
         #Locate end of section 1
         while macdh[idx] <= 0 and idx < dsize:
             idx += 1
-        if idx == dsize:
+        if idx == dsize or idx <= min_sec_len:
             return False
         pos.append(idx)
         #Locate end of section 2
         while macdh[idx] >= 0 and idx < dsize:
             idx += 1
-        if idx == dsize:
+        if idx == dsize or (idx - pos[len(pos)-1]) <= min_sec_len:
             return False
         pos.append(idx)
         #Locate end of section 3
         while macdh[idx] <= 0 and idx < dsize:
             idx += 1
-        if idx == dsize:
+        if idx == dsize or (idx - pos[len(pos)-1]) <= min_sec_len:
             return False
         pos.append(idx)
         assert(len(pos) == 3)
